7a819e24a5181635fe2852631a96db52dc40f603
droooney <d.vor7@mail.ru>
2025-10-08 13:03:39 +0500

    script

--- yandex-mail-theme.js
+++ yandex-mail-theme.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      2025-10-08
+// @version      1.0.0
+// @updateURL    https://raw.githubusercontent.com/droooney/custom-scripts/refs/heads/master/yandex-mail-theme.js
+// @downloadURL  https://raw.githubusercontent.com/droooney/custom-scripts/refs/heads/master/yandex-mail-theme.js
 // @description  try to take over the world!
 // @author       You
 // @include      https://mail.yandex.com
@@ -10,5 +12,54 @@
 // ==/UserScript==
 
 (() => {
-  console.log(234);
+  const match = window.matchMedia('(prefers-color-scheme: dark)');
+
+  const getSettingsButton = () => {
+    return document.querySelector('button[data-testid="settings-button"]');
+  };
+
+  const poll = (checker) => {
+    return new Promise((resolve, reject) => {
+      let tries = 0;
+
+      const check = () => {
+        try {
+          if (++tries > 100) {
+            throw new Error('Timeout');
+          }
+
+          const result = checker();
+
+          if (result) {
+            resolve(result);
+          } else {
+            setTimeout(check, 50);
+          }
+        } catch (err) {
+          reject(err);
+        }
+      };
+
+      check();
+    });
+  };
+
+  const setTheme = async () => {
+    const settingsButton = await poll(getSettingsButton);
+    const isDarkMode = match.matches;
+
+    settingsButton.click();
+
+    await poll(() => document.querySelector('[data-testid="settings-popup"]'));
+
+    const cards = [...document.querySelectorAll('[class*="ThemesWithScope__card"]')];
+    const card = isDarkMode ? cards.at(2) : cards.at(0);
+
+    card?.click();
+    settingsButton.click();
+  };
+
+  setTheme();
+
+  match.addEventListener('change', setTheme);
 })();
