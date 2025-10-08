8f4508f56e2e93e9cbfcf311c3cbd1d4e68a4257
droooney <d.vor7@mail.ru>
2025-10-08 12:11:34 +0500

    init

---
+++ yandex-mail-theme.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-10-08
+// @description  try to take over the world!
+// @author       You
+// @include      https://mail.yandex.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=yandex.com
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  console.log(123);
+})();
